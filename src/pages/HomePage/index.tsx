cd204c5f02e002b6664699a4e01dda4a8b2f82d0
Otavio Henrique <42388160+otaviohenrique1@users.noreply.github.com>
2022-01-25 22:49:00 -0300

    Commit 3 dos arquivos

--- src/pages/HomePage/index.tsx
+++ src/pages/HomePage/index.tsx
@@ -1,10 +1,19 @@
-import { ContainerApp } from "../../components/ContainerApp";
-import { Titulo } from "../../components/Titulo";
+import { Col, Row } from "reactstrap";
+import { HeaderApp } from "../../components/HeaderApp";
 
 export function HomePage() {
   return (
-    <ContainerApp>
-      <Titulo tag="h1">HomePage</Titulo>
-    </ContainerApp>
+    <div className="h-100">
+      <HeaderApp />
+      <Row className="h-100 p-0 m-0">
+        <Col md={2} className="bg-light border-end">q</Col>
+        <Col md={10} className="bg-light">
+          <Row>
+            <Col md={12}></Col>
+            <Col md={12}></Col>
+          </Row>
+        </Col>
+      </Row>
+    </div>
   );
 }
